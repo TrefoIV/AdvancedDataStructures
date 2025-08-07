e4b6db45918bd56b2ca92f149c78f2a788eb83d8
TrefoIV <57830785+TrefoIV@users.noreply.github.com>
2025-08-07 12:38:27 +0200

    Added ToConcatenatedList extension

---
+++ AdvancedDataStructures/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using AdvancedDataStructures.ConcatenatedList;
+
+namespace AdvancedDataStructures.Extensions
+{
+	public static class IEnumerableExtensions
+	{
+		public static ConcatenatedLinkedList<T> ToConcatenatedList<T>(this IEnumerable<T> source)
+		{
+			return new ConcatenatedLinkedList<T>(source);
+		}
+	}
+}
