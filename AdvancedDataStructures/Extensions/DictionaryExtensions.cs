2778505c28c59cd97f7a416b3a57b36db0df37f9
TrefoIV <57830785+TrefoIV@users.noreply.github.com>
2025-09-22 10:48:10 +0200

    Added GetOrInsert extension method for dictionaries

---
+++ AdvancedDataStructures/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace AdvancedDataStructures.Extensions
+{
+	public static class DictionaryExtensions
+	{
+		public static TValue GetOrInsert<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue def)
+		{
+			if (dict.TryGetValue(key, out TValue value))
+			{
+				return value;
+			}
+			dict[key] = def;
+			return def;
+		}
+	}
+}
